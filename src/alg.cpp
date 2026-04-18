d5329ea2131227f5dbb2353a020d8d42e77b37d6
NikitaSemerikov <167699682+NikitaSemerikov@users.noreply.github.com>
2026-04-18 16:19:10 +0300

    Семериков_25-ИСТ-3.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,80 @@
 // Copyright 2025 NNTU-CS
 #include <string>
-#include <map>
+#include <cctype>
 #include "tstack.h"
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+int priority(char op) {
+  if (op == '+' || op == '-') return 1;
+  if (op == '*' || op == '/') return 2;
+  return 0;
 }
 
-int eval(const std::string& pref) {
-  // добавьте код
+int apply(int a, int b, char op) {
+  switch (op) {
+    case '+': return a + b;
+    case '-': return a - b;
+    case '*': return a * b;
+    case '/': return a / b;
+  }
   return 0;
+}
+
+std::string infx2pstfx(const std::string& inf) {
+  TStack<char, 256> st;
+  std::string out;
+
+  for (size_t i = 0; i < inf.size(); ++i) {
+    char c = inf[i];
+
+    if (std::isdigit(c)) {
+      while (i < inf.size() && std::isdigit(inf[i])) {
+        out += inf[i++];
+      }
+      out += ' ';
+      --i;
+    } else if (c == '(') {
+      st.put(c);
+    } else if (c == ')') {
+      while (!st.isEmpty() && st.see() != '(') {
+        out += st.get();
+        out += ' ';
+      }
+      st.get();  // remove '('
+    } else if (!std::isspace(c)) {
+      while (!st.isEmpty() && priority(st.see()) >= priority(c)) {
+        out += st.get();
+        out += ' ';
+      }
+      st.put(c);
+    }
+  }
+
+  while (!st.isEmpty()) {
+    out += st.get();
+    out += ' ';
+  }
+
+  return out;
+}
+
+int eval(const std::string& post) {
+  TStack<int, 256> st;
+
+  for (size_t i = 0; i < post.size(); ++i) {
+    if (std::isdigit(post[i])) {
+      int x = 0;
+      while (i < post.size() && std::isdigit(post[i])) {
+        x = x * 10 + (post[i] - '0');
+        ++i;
+      }
+      st.put(x);
+      --i;
+    } else if (!std::isspace(post[i])) {
+      int b = st.get();
+      int a = st.get();
+      st.put(apply(a, b, post[i]));
+    }
+  }
+
+  return st.get();
 }
